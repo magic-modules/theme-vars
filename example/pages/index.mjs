22054c7b8894e88ccd3b3ba8402256d85e5c0be3
jascha <jascha@jaeh.at>
2020-02-12 00:09:13 +0100

    example: add state info to docs

--- example/pages/index.mjs
+++ example/pages/index.mjs
@@ -18,7 +18,39 @@ export const View = state => [
     ' file to the /app.mjs file',
   ]),
 
-  Pre('export const View = (state) => ThemeVars(state)'),
+  Pre(`
+/app.mjs
+export const state = {
+  // ...other state,
+  vars: {
+    background: {
+      dark: '#232323',
+      light: '#eeeeee',
+    },
+    text: {
+      dark: '#cccccc',
+      light: '#232323',
+    },
+    link: {
+      dark: '#eeeeee',
+      light: '#010101',
+      hover: {
+        dark: '#fefefe',
+        light: '#010101',
+      },
+    },
+    fadeDuration: '300ms',
+    widths: {
+      laptop: '1000px',
+    },
+  },
+}
+`),
+
+  Pre(`
+/pages/index.mjs
+export const View = (state) => ThemeVars(state.vars)
+`),
 
   h2('demo'),
   ThemeVars(state.vars),
